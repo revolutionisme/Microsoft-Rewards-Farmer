66b741b670ab94d0253f408d696cefbb0f00bee4
Biswas Biplob <Biplob.Biswas@burda-forward.de>
2021-06-06 03:42:24 +0200

    add options to fix error of chromedriver

--- src/rewards_lambda.py
+++ src/rewards_lambda.py
@@ -151,6 +151,8 @@ def browserSetup(
     # for argument in lambda_options:
     #     options.add_argument(argument)
 
+    options.add_arguments("--no-sandbox")
+    options.add_arguments("--disable-dev-shm-usage")
     options.add_argument("user-agent=" + user_agent)
     options.add_argument("lang=" + LANG.split("-")[0])
     if headless_mode:
